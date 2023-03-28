4935c4543162718ef263848a71ae5837396b4765
Владислав Горский <vg07102004@gmail.com>
2023-03-28 15:37:09 +0300

    Progress backup 1
    - Block
    - BlockButton
    - BlockCactus
    - BlockDoor
    - IInventory

---
+++ server/inject/net/minecraft/server/BlockCactus.java
@@ -0,0 +1,7 @@
+package net.minecraft.server;
+
+public class BlockCactus extends net.minecraft.src.BlockCactus {
+    protected BlockCactus(int id, int blockIndex) {
+        super(id, blockIndex);
+    }
+}
---
+++ server/inject/net/minecraft/server/BlockDoor.java
@@ -0,0 +1,9 @@
+package net.minecraft.server;
+
+import net.minecraft.src.Material;
+
+public class BlockDoor extends net.minecraft.src.BlockDoor {
+    protected BlockDoor(int id, Material material) {
+        super(id, material);
+    }
+}
