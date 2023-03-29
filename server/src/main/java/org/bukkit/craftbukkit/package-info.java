8af8c184713aa635e77e56d5917cd15f95d1c1fc
Владислав Горский <vg07102004@gmail.com>
2023-03-29 07:57:35 +0300

    CB Code added
    - Removed RML from Runtime
    - Removed RosepadLoader from Runtime
    - Added CB dependencies

---
+++ server/src/main/java/org/bukkit/craftbukkit/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The following package contains code of CraftBukkit server core, commit cc635ed51c8a98bbe2088115ca617c76cd7c730a
+ * <p>
+ * https://hub.spigotmc.org/stash/projects/SPIGOT/repos/craftbukkit/commits/cc635ed51c8a98bbe2088115ca617c76cd7c730a
+ */
+package org.bukkit.craftbukkit;
